df6acb5dc0403e70b71af3bbcbbc33eab38f5808
davidMkCodes <72777029+davidMkCodes@users.noreply.github.com>
2023-04-14 16:48:17 -0700

    Add files via upload

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,57 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    public UserDaoService service;
+
+    public UserResource(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if(user == null)
+            throw new UserNotFoundException("id:"+id);
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
+    }
+
+    //GET /users/{id}
+    @DeleteMapping("/users/{id}")
+    public void DeleteUser(@PathVariable int id){
+        service.deleteById(id);
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+}
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'David');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Dave');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Davit');
+
+insert into post(id, description, user_id)
+values(20001, 'Learn AWS', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'Learn Devops', 10002);
+
+insert into post(id, description, user_id)
+values(20003, 'Learn Multi Cloud', 10003);
